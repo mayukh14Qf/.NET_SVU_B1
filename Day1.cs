4d84d5c8f2b5951e49551b76decd0090e79234a2
ABC <mayukc55@gmail.com>
2024-06-25 15:40:48 +0530

    first commit

---
+++ Day1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCA_B1_207
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello World :)");
+            Console.Write(6+4);
+            Console.WriteLine(" Hi, In Ch sharp");
+
+
+
+        }
+    }
+}
