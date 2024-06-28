35a0735c4a12be6ac2b2a837eff4dcab6633b7cc
ABC <mayukc55@gmail.com>
2024-06-28 15:50:48 +0530

    day 4

---
+++ Day4.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCA_B1_207
+{
+    public class Day4
+    {
+        static void Main(string[] args)
+        {
+            // Simple Interest
+
+            double principle = 1234;
+            double rateOfInterest = 4;
+            double time = 3;
+
+            double simpleInterest = (principle * rateOfInterest * time) / 100;
+            Console.WriteLine(simpleInterest);
+
+            //Swap two Numbers With Third variable
+
+            int a = 10;
+            int b = 40;
+
+            int c = a; // c=10
+            a = b; // a=40
+            b = c; // b=10
+
+            Console.WriteLine("your A is : " + a);
+            Console.WriteLine("Your B is : " + b);
+
+            //Swap two Numbers WithOut Using Third variable(addition and subtraction)
+
+            int x = 30;
+            int y = 50;
+
+            x = x + y; // x=80 , y=50
+            y = x - y; // x=80, y=30
+            x = x - y; // x=50 , y=30
+
+            Console.WriteLine("your X is : " + x);
+            Console.WriteLine("Your Y is : " + y);
+
+            //Swap two Numbers WithOut Using Third variable(Multiplication and Division)
+
+            int p = 30;
+            int q = 70;
+
+            p = p * q; // p = 30*70, q=70
+            q = p / q; // p = 30*70, q=30
+            p = p / q; // p=70, q=30 
+
+            Console.WriteLine("Your P is :" + p);
+            Console.WriteLine("Your Q is :" + q);
+
+
+            // Print the Last digit of a number
+
+            int number1 = 45678;
+            int lastDigit = (number1 % 10);
+            Console.WriteLine(lastDigit); // ans : 8
+
+
+            // sum of n natural Numbers 
+            // (10+9+8+7+6+5+4+3+2+1)=55
+
+            int n = 10;
+
+            int result = (n * (n + 1)) / 2;
+
+            Console.WriteLine(result);
+
+
+            // Relational Operators 
+            // It will give you boolean output, either true or false
+
+
+            int num1= 10;
+            int num2 = 20;
+
+            Console.WriteLine(num1 == num2); // False
+
+            bool check1 = (num1 != num2);
+            Console.WriteLine(check1); // True
+
+
+            Console.WriteLine(num2 >= 20); // True
+            Console.WriteLine(num2 > 20); // False
+
+
+            // Logical Operators
+            // and( && ) , or (||) , not ( ! )
+
+
+            Console.WriteLine(num2 == 20 && num2 > 20 && num1 == 10);
+
+            Console.WriteLine(num2 == 20 || num2 > 20 || num1 == 10);
+
+            bool check2 = !(num1 != num2);
+            Console.WriteLine(check2);
+
+
+            // Relational and logical will check the conditions 
+            // if else, loops, so on
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
