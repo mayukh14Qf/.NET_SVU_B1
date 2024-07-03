e40a9a4b42c9181979e37d244a0235d53e6a1437
ABC <mayukc55@gmail.com>
2024-07-03 16:08:04 +0530

    day 7 done

--- Day7.cs
+++ Day7.cs
@@ -29,18 +29,67 @@ namespace BCA_B1_207
             else if (number1 > number2)
             {
                 Console.WriteLine("Your Greater number is " + number1);
-            }
+            } 
             else {
                 Console.WriteLine("Your Greater number is " + number2);
             }
 
+            // Positive or not
+
+            Console.WriteLine("Enter A Number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number == 0)
+            {
+                Console.WriteLine("Enter a number which is not zero");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine("The number is Positive");
+            }
+            else
+            {
+                Console.WriteLine("The Number is Negative");
+            }
+
+            // Grading system using if else
+
+            Console.WriteLine("Enter Your Number in Bengali");
+            double bengali = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Number in English");
+            double english = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Number in Hindi");
+            double hindi = Convert.ToDouble(Console.ReadLine());
 
 
+            double avg = (bengali + english + hindi) / 3;
 
-
-
-
-
+            if (avg > 90)
+            {
+                Console.WriteLine("A+");
+            }
+            else if (avg >= 80 && avg <= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (avg >= 70 && avg < 80)
+            {
+                Console.WriteLine("B+");
+            }
+            else if (avg >= 60 && avg <= 69)
+            {
+                Console.WriteLine("B");
+            }
+            else if (avg >= 50 && avg < 60)
+            {
+                Console.WriteLine("C");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
 
         }
     }
